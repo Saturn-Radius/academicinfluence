416985cc85f569b725258f6d9ce72a3708b1f157
polaris-labs <brave.tiger1227@gmail.com>
2019-12-20 15:37:56 -0800

    using image gallery

--- pages/api/image/[slug].ts
+++ pages/api/image/[slug].ts
@@ -1,17 +1,23 @@
 import { NextApiResponse, NextApiRequest } from "next";
 import databasePool from "../../../databasePool"
+import * as squel from "../../../squel"
 
 export default async (req: NextApiRequest, response: NextApiResponse) => {
    try {
 
         const pool = await databasePool;
 
-        const hash = req.query.hash;
+        const slug = req.query.slug;
 
-            const db_response = await pool.query({
-                text: "SELECT data, type FROM editor.BLOBS WHERE ID = $1",
-                values: [hash]
-            });
+            const db_response = await pool.query(
+                squel.select()
+                    .from("editor.images")
+                    .join("editor.blobs", undefined, "images.image = blobs.id")
+                    .where("editor.images.slug = ?", slug)
+                    .field("data")
+                    .field("type")
+                    .toParam()
+            );
 
             if (db_response.rows.length == 0) {
                 response.status(404)
