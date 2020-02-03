556f047e2abb58a59afff553c982effe08a4bbc9
polaris-labs <brave.tiger1227@gmail.com>
2020-02-03 08:47:01 -0800

    support dashes for spaces

--- service/schoolSubjectPage.tsx
+++ service/schoolSubjectPage.tsx
@@ -167,7 +167,7 @@ export default async function serveSchoolSubjectPage(request: SchoolSubjectPageR
 
     const baseQuery = influenceScoreQuery("person", 1900, 2020)
         .join("editor.ai_disciplines", undefined, "ai_disciplines.id = scores.keyword")
-        .where("lower(ai_disciplines.name) = lower(?)", request.discipline)
+        .where("lower(ai_disciplines.name) = lower(?)", request.discipline.replace(/-/g, ' '))
         .join("ai_data.person_schools", undefined, "person_schools.person_id = scores.id")
         .join("editor.ai_schools", undefined, "editor.ai_schools.id = person_schools.school_id")
         .where("ai_schools.slug = ?", request.slug)
