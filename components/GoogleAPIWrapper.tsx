db85fa5ae61473cff15d128a52d8fd9021dad1be
polaris-labs <brave.tiger1227@gmail.com>
2020-03-06 04:47:32 -0500

    Added custom common shared google api wrapper component

---
+++ components/GoogleAPIWrapper.tsx
@@ -0,0 +1,17 @@
+import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
+
+const MapContainer = (props: any) => {
+  const { google } = props;
+
+  return (
+    <Map google={google} zoom={14} mapTypeControl={false}>
+      <Marker />
+
+      {/* <InfoWindow visible={true} /> */}
+    </Map>
+  );
+};
+
+export default GoogleApiWrapper({
+  apiKey: process.env.GOOGLE_MAP_KEY || ""
+})(MapContainer);
