4f109efb5c6cb84f047a86240680e64f764c5485
polaris-labs <brave.tiger1227@gmail.com>
2020-03-08 15:20:27 -0400

    Added schools styled components for layout build

---
+++ components/schools/styles.tsx
@@ -0,0 +1,13 @@
+import css from "@emotion/css";
+import styled from "@emotion/styled";
+import { Col } from "../grid";
+
+const LeftCol = styled(Col)`
+  width: 75%;
+`;
+
+const RightCol = styled(Col)`
+  width: 25%;
+`;
+
+export { LeftCol, RightCol };
