36c46049439ef4982a8515e4290c19184273ad6a
polaris-labs <brave.tiger1227@gmail.com>
2020-03-09 15:53:34 -0400

    Modified schools list top menu component with the use advanced search mode toggle feature

--- components/schools/ListTopMenu.tsx
+++ components/schools/ListTopMenu.tsx
@@ -1,16 +1,35 @@
+import { useState } from "react";
 import styled from "@emotion/styled";
 import { Col } from "../grid";
 import SchoolSearchBox from "./SchoolSearchBox";
+import Discipline from "./Discipline";
+import YearsFilter from "./YearsFilter";
+import Country from "./Country";
 
 const Wrapper = styled.div`
   min-width: 100%;
   min-height: 40px;
 `;
 
-const ListTopMenu = () => (
-  <Wrapper>
-    <SchoolSearchBox />
-  </Wrapper>
-);
+const ListTopMenu = (props: any) => {
+  const [isAdvancedMode, setIsAdvanced] = useState(false);
+
+  const onSearchModeHandler = (isAdvanced: boolean) => {
+    setIsAdvanced(isAdvanced);
+  };
+
+  return (
+    <Wrapper>
+      <SchoolSearchBox onAdvancedSearchClick={onSearchModeHandler} />
+      {isAdvancedMode && (
+        <>
+          <Discipline {...props} />
+          <YearsFilter {...props} />
+          <Country {...props} />
+        </>
+      )}
+    </Wrapper>
+  );
+};
 
 export default ListTopMenu;
