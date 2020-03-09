530f811d875e5fb84571e22b4a376794ac7f3b87
polaris-labs <brave.tiger1227@gmail.com>
2020-03-08 22:58:34 -0400

    Modified school search box component

--- components/schools/SchoolSearchBox.tsx
+++ components/schools/SchoolSearchBox.tsx
@@ -4,12 +4,50 @@ import { useRouter } from "next/router";
 import Autocomplete from "react-autocomplete";
 import { apiSchoolSearch } from "../../api";
 import { Identifiable } from "../../schema";
+import { GRAY_DARK, GRAY_MID } from "../../styles";
 
 const Search = styled(Autocomplete)`
   input {
     width: 100%;
   }
 `;
+
+interface SearchListItemProps {
+  readonly isHighlighted: boolean;
+}
+const SearchListItem = styled.div<SearchListItemProps>`
+  width: 100%;
+  background: ${props => (props.isHighlighted ? "lightgray" : "white")};
+`;
+
+const wrapperStyle = {
+  width: "100%",
+  minHeight: "39px"
+};
+
+const menuStyle = {
+  zIndex: 1,
+  border: `solid 1px ${GRAY_DARK}`,
+  borderRadius: "3px"
+};
+
+const inputStyle = {
+  width: "100%",
+  height: "100%",
+  maxHeight: "50%",
+  backgroundColor: "#ffffff",
+  borderRadius: "3px",
+  padding: "2px 0",
+  textIndent: "10px",
+  overflow: "auto",
+  outline: "none",
+  fontFamily: "SF UI Display Medium",
+  fontSize: "12px",
+  fontStyle: "normal",
+  lineHeight: "normal",
+  letterSpacing: "normal",
+  color: `${GRAY_MID}`
+};
 
 const SchoolSearchBox = () => {
   const [value, setValue] = useState("");
@@ -41,30 +79,15 @@ const SchoolSearchBox = () => {
       onChange={onChange}
       onSelect={onSelect}
       getItemValue={item => item.slug}
-      wrapperStyle={{
-        width: "100%"
-      }}
-      menuStyle={{
-        borderRadius: "3px",
-        boxShadow: "0 2px 12px rgba(0, 0, 0, 0.1)",
-        background: "rgba(255, 255, 255, 0.9)",
-        padding: "2px 0",
-        fontSize: "90%",
-        position: "fixed",
-        overflow: "auto",
-        maxHeight: "50%",
-        zIndex: 1
+      wrapperStyle={wrapperStyle}
+      menuStyle={menuStyle}
+      inputProps={{
+        style: inputStyle
       }}
       renderItem={(item, isHighlighted) => (
-        <div
-          key={item.slug}
-          style={{
-            width: "100%",
-            background: isHighlighted ? "lightgray" : "white"
-          }}
-        >
+        <SearchListItem key={item.slug} isHighlighted={isHighlighted}>
           {item.name}
-        </div>
+        </SearchListItem>
       )}
     />
   );
