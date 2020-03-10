3350d6f765d79e06a842f10060eb6283a8609b6c
polaris-labs <brave.tiger1227@gmail.com>
2020-03-09 22:59:17 -0400

    Modified school list component with the custom defined school list item component

--- components/schools/SchoolList.tsx
+++ components/schools/SchoolList.tsx
@@ -1,13 +1,15 @@
 import SchoolListItem from "./SchoolListItem";
 
 interface SchoolListProps {
-  mode: String;
-  schools: any[];
+  mode: string;
+  schools: any;
 }
 const SchoolList = (props: SchoolListProps) => {
   const { mode, schools } = props;
 
-  return schools.map(item => <SchoolListItem mode={mode} school={item} />);
+  return schools.map((item: any) => (
+    <SchoolListItem mode={mode} school={item} />
+  ));
 };
 
 export default SchoolList;
