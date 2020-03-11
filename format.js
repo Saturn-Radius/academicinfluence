4a872668eca782d0fc89562e7d28b36b59b7ba20
polaris-labs <brave.tiger1227@gmail.com>
2020-03-11 08:24:01 -0700

    format check 2

--- format.js
+++ format.js
@@ -1,1 +1,12 @@
-console.log(process.argv)
+const { readFileSync, writeFileSync } = require("fs");
+const { format } = require("prettier");
+
+for (const filename of process.argv.slice(2)) {
+  const source = readFileSync(filename, "utf-8");
+  writeFileSync(
+    filename,
+    format(source, {
+      parser: "babel"
+    })
+  );
+}
