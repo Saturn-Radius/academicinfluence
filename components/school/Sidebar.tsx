1e1325ac9d05d87d9274d5f2aef9649de96c3a5b
polaris-labs <brave.tiger1227@gmail.com>
2020-03-13 11:07:01 -0700

    resolved forward ref warning for sidebar

--- components/school/Sidebar.tsx
+++ components/school/Sidebar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import React from "react";
 import { MAIN } from "../../styles";
 
 const Sidebar = () => {
@@ -19,27 +20,33 @@ const Sidebar = () => {
   );
 };
 
-const SidebarButton = (props: {
-  img: string;
-  text: string;
-  onClick?: () => void;
-}) => {
-  return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        boxShadow: "0 4px 4px 0 rgba(0, 0, 0, 0.25)",
-        padding: "12px 17px",
-        marginBottom: 20
-      }}
-      onClick={props.onClick}
-    >
-      <img style={{ width: 60, height: 60 }} src={props.img} />
-      <span style={styles.sidebarText}>{props.text}</span>
-    </div>
-  );
-};
+const SidebarButton = React.forwardRef(
+  (
+    props: {
+      img: string;
+      text: string;
+      onClick?: () => void;
+    },
+    ref: any
+  ) => {
+    return (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          boxShadow: "0 4px 4px 0 rgba(0, 0, 0, 0.25)",
+          padding: "12px 17px",
+          marginBottom: 20
+        }}
+        onClick={props.onClick}
+        ref={ref}
+      >
+        <img style={{ width: 60, height: 60 }} src={props.img} />
+        <span style={styles.sidebarText}>{props.text}</span>
+      </div>
+    );
+  }
+);
 
 const styles = {
   sidebarText: {
