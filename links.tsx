e141a23956a1ac60d60b170b271785efbe798dad
polaris-labs <brave.tiger1227@gmail.com>
2020-03-13 18:37:24 -0700

    correction to person links

--- links.tsx
+++ links.tsx
@@ -47,11 +47,7 @@ export function PersonLink(props: {
   children: React.ReactNode;
 }) {
   return (
-    <Link
-      href={`/people/${props.person.slug}`}
-      as={`/people/${props.person.slug}`}
-      passHref
-    >
+    <Link href={`/people/[slug]`} as={`/people/${props.person.slug}`} passHref>
       {props.children}
     </Link>
   );
