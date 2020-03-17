cf2cbf67ecbd9950d84319a7f1dc0aa04b7a1c3f
polaris-labs <brave.tiger1227@gmail.com>
2020-03-17 09:26:36 -0700

    add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import { apiFeaturesPage, apiHomePage, apiInfluentialPeoplePage } from "../api";
 import BacktotopButton from "../components/BacktotopButton";
 import { useBasicContext } from "../components/BasicContext";
@@ -26,6 +27,163 @@ const Index: NextPage<IndexProps> = (props: IndexProps) => {
   const basicContext = useBasicContext();
   return (
     <div>
+      <Head>
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#e57045" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon-196x196.png" sizes="196x196" />
+        <link
+          rel="shortcut icon"
+          href="/android-chrome-192x192.png"
+          sizes="192x192"
+        />
+        <link
+          rel="shortcut icon"
+          href="/android-chrome-512x512.png"
+          sizes="512x512"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-228x228.png"
+          sizes="228x228"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-196x196.png"
+          sizes="196x196"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-192x192.png"
+          sizes="192x192"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-160x160.png"
+          sizes="160x160"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-144x144.png"
+          sizes="144x144"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-128x128.png"
+          sizes="128x128"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-120x120.png"
+          sizes="128x128"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-32x32.png"
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-16x16.png"
+          sizes="16x16"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon.png"
+          sizes="180x180"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon-180x180.png"
+          sizes="180x180"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon-152x152.png"
+          sizes="152x152"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          href="/apple-touch-icon-152x152.png"
+          sizes="152x152"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          href="/apple-touch-icon-144x144.png"
+          sizes="144x144"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-touch-icon-120x120.png"
+          sizes="120x120"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          href="/apple-touch-icon-120x120.png"
+          sizes="120x120"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          href="/apple-touch-icon-114x114.png"
+          sizes="114x114"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          href="/apple-touch-icon-76x76.png"
+          sizes="76x76"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          href="/apple-touch-icon-72x72.png"
+          sizes="72x72"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          href="/apple-touch-icon-60x60.png"
+          sizes="60x60"
+        />
+        <link
+          rel="apple-touch-icon-precomposed"
+          href="/apple-touch-icon-57x57.png"
+          sizes="57x57"
+        />
+        <meta name="msapplication-TileColor" content="#5997A2" />
+        <meta name="theme-color" content="#5997A2" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#FFFFFF" />
+        <meta
+          name="msapplication-wide310x150logo"
+          content="/mstile-310x150.png"
+        />
+        <meta
+          name="msapplication-square310x310logo"
+          content="/mstile-310x310.png"
+        />
+        <meta
+          name="msapplication-square150x150logo"
+          content="/mstile-150x150.png"
+        />
+        <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
+        <meta
+          name="msapplication-square70x70logo"
+          content="/mstile-70x70.png"
+        />
+      </Head>
       <style jsx>
         {`
           .maincontent {
