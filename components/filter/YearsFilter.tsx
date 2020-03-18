7000dc43024d6409bd009af5bc6e092d34b2d21b
polaris-labs <brave.tiger1227@gmail.com>
2020-03-18 11:00:16 -0700

    added labels to years

--- components/filter/YearsFilter.tsx
+++ components/filter/YearsFilter.tsx
@@ -1,3 +1,4 @@
+import { Dictionary } from "lodash";
 import { Range } from "rc-slider";
 import "rc-slider/assets/index.css";
 import { useCallback } from "react";
@@ -5,6 +6,17 @@ import { ERAS, formatYear } from "../../utils/years";
 import FilterLabel from "../schools/FilterLabel";
 import RangeHandle from "../schools/RangeHandle";
 import { FilterProps } from "./types";
+
+function makeMarks(startYear: number) {
+  const values: Dictionary<string> = {};
+  const offset = ERAS.indexOf(startYear);
+  for (let index = offset; index < ERAS.length; index++) {
+    if ((index - offset) % 4 == 0) {
+      values[`${index}`] = formatYear(ERAS[index]);
+    }
+  }
+  return values;
+}
 
 function YearsFilter<
   R extends {
@@ -40,6 +52,7 @@ function YearsFilter<
           format: year => formatYear(ERAS[year])
         })}
         onChange={onChange}
+        marks={makeMarks(props.minYear)}
       />
     </FilterLabel>
   );
